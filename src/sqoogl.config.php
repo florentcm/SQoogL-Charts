c9bf65feb8dea6266b3ebf159f2400ea64255ffa
Florent Cima <florentcm@gmail.com>
2011-10-14 12:02:46 -0400

    Innitial Commit
    Add support for all graph of Google Charts Gallery :
    http://code.google.com/intl/en-EN/apis/chart/image/docs/gallery/chart_ga
    ll.html

---
+++ src/sqoogl.config.php
@@ -0,0 +1,16 @@
+<?php
+/**  @file
+  *  PHP SQoogL config
+  *
+  *  @package	sqoogl
+  *  @updated	2011/10/13
+  *  @author	Florent Cima <florent.cima@gmail.com>
+  *  @copyright	2011-present, Florent Cima
+  *  @license	GNU General Public License (GPL)
+  *
+  *  GLOBALS
+  */
+  define('SQOOGLE_HOST', 'localhost');
+  define('SQOOGLE_USER', 'USER_NAME');
+  define('SQOOGLE_PASSWORD', 'YOUR_PASSWORD');
+  define('SQOOGLE_DATABASE', 'drupal7');
